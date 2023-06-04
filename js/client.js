fe77a38fb2c21830765553489a53a58e5fd9b29c
Aaron Butnik <aaronjbutnik@gmail.com>
2023-06-04 02:02:51 -0700

    working on bugs.

--- js/client.js
+++ js/client.js
@@ -1,14 +1,14 @@
-const input = document.querySelector("input").value;
 const button = document.querySelector("button");
 
 button.addEventListener("click", getRapper);
 
 async function getRapper() {
+  const input = document.querySelector("input").value;
   try {
-    const request = await fetch(
+    const response = await fetch(
       `https://full-stack-practice-fnfr.onrender.com/api/${input}`
     );
-    const data = await request.json();
+    const data = await response.json();
     console.log(data);
     document.querySelector("#age").innerText = data.age;
     document.querySelector("#birthname").innerText = data.birthname;
